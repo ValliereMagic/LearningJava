51bf1028203ed3395a17e4297a2ec277854a71e3
ValliereMagic <louisemelaney@outlook.com>
2016-08-12 11:01:13 -0400

    Learning about Key Words and Expressions

---
+++ Keywords_Expressions/Keywords_Expressions/src/net/strangled/maladan/Main.java
@@ -0,0 +1,21 @@
+package net.strangled.maladan;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// a mile is equal to 1.609344
+		double kilometeres = (100 * 1.609344);
+		System.out.println(kilometeres);
+		int highscore =50;
+		if (highscore ==50) {
+			System.out.println("This is an expression.");
+			//expressions from above
+			//score = 100
+			//score > 99
+			//"You got the high score!"
+			//score = 0
+		}
+		
+	}
+
+}
