92667a35f5d7b013a05183ff99aaa7b4a644b557
ValliereMagic <ValliereMagic@users.noreply.github.com>
2016-08-12 10:58:20 -0400

    Learning about Overloading Methods

---
+++ Overloading Methods/Method_Overloading/src/net/strangled/maladan/Main.java
@@ -0,0 +1,43 @@
+package net.strangled.maladan;
+
+public class Main {
+
+	public static void main(String[] args) {
+	calculateScore("ValliereMagic", 500);
+	calculateScore(5000);
+	calculateScore(12);
+	calcFeetAndInches(5, 10);	
+	calcFeetAndInches(70);
+	}
+	public static int calculateScore(String playerName, int score) {
+		System.out.println("Player" + playerName + " Scored " + score + " Points.");
+		return score * 1000;
+	}
+	public static int calculateScore(int score) {
+	System.out.println("Unknown Plaver" + " Scored " + score + " Points.");
+	return score * 1000;
+	}
+	public static int calculateScore() {
+		return 0;
+	}
+//Number of Parameters must change for method overloading.
+	
+	//Challenge
+	public static double calcFeetAndInches(double feetToInches, double inches) {
+		if (feetToInches >= 0d && inches >=0d && inches <=12d) {
+			feetToInches = feetToInches * 12d;
+			double totalCentimeteres = (feetToInches + inches) * 2.54;
+			System.out.println("Total Centimeteres: " + totalCentimeteres);
+			return totalCentimeteres;
+		}
+			return -1d;
+	}	
+	public static double calcFeetAndInches(double inches) {
+		if (inches >=0) {
+			double feet = inches / 12d;
+			double answer =calcFeetAndInches(feet, 0d);
+			return answer;
+		}
+			return -1d;
+	}
+}	
