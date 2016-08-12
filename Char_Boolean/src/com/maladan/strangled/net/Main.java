f1d2d6f042b9942d5244c73e286b7ca3c6ee45b5
ValliereMagic <ValliereMagic@users.noreply.github.com>
2016-08-12 11:03:09 -0400

    Characters and Booleans

---
+++ Char_Boolean/src/com/maladan/strangled/net/Main.java
@@ -0,0 +1,15 @@
+package com.maladan.strangled.net;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char myChar = '\u00A9';
+		char Registered = '\u00AE';
+		System.out.println(myChar);
+		System.out.println(Registered);
+		
+		boolean myboolean = true;
+		System.out.println(myboolean);
+	}
+
+}
