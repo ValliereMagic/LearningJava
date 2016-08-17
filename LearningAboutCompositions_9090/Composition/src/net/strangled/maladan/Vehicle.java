36ad4745b6bcd24d8c00f72b84d5781aeb65d694
ValliereMagic <louisemelaney@outlook.com>
2016-08-16 22:18:24 -0400

    Learning how composition works.

---
+++ LearningAboutCompositions_9090/Composition/src/net/strangled/maladan/Vehicle.java
@@ -0,0 +1,11 @@
+package net.strangled.maladan;
+
+public class Vehicle {
+	private String name;
+	public Vehicle(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
