3793a2a8a5194ef88b6be0a1a46bf8e80b495616
Louise <Louise@DESKTOP-3LE7GH9>
2016-08-28 16:49:52 -0400

    Made command line calculator

---
+++ MalaCalc/JavaCalc/src/net/strangled/maladan/Calculate.java
@@ -0,0 +1,23 @@
+package net.strangled.maladan;
+
+public class Calculate {
+	public double add(double a, double b) {
+		double c = a + b;
+		return c;
+	}
+	public double subtract(double a, double b) {
+		double c = a - b;
+		return c;
+	}
+	public double multiply(double a, double b) {
+		double c = a * b;
+		return c;
+	}
+	public double divide(double a, double b) {
+		double c = a / b;
+		return c;
+	}
+	public double quadratic(double a, double b) {
+		return -1;
+	}
+}
---
+++ MalaCalc/JavaCalc/src/net/strangled/maladan/Choice.java
@@ -0,0 +1,72 @@
+package net.strangled.maladan;
+
+public class Choice extends Main{
+	private int choice;
+	private boolean running;
+	private double a;
+	private double b;
+	private double c;
+	public Choice() {
+		System.out.println("Enter your choice: ");
+		this.choice = input.nextInt();
+		input.nextLine();
+		if(this.choice == 7) {
+			this.running = false;
+		} else {
+			this.running = true;
+		}
+	}
+	public int getChoice() {
+		return choice;
+	}
+	public boolean isRunning() {
+		return running;
+	}
+	public void calculate(int choice) {
+		Calculate calculate = new Calculate();
+		switch(choice) {
+			case 1:
+				System.out.println("Enter the two numbers to add.");
+				getTwoNumbers();
+				System.out.println("The answer is: " + calculate.add(this.a, this.b));
+				break;
+			case 2:
+				System.out.println("Enter the two numbers to subtract.");
+				getTwoNumbers();
+				System.out.println("The answer is: " + calculate.subtract(this.a, this.b));
+				break;
+			case 3:
+				System.out.println("Enter the two numbers to multiply.");
+				getTwoNumbers();
+				System.out.println("The answer is: " + calculate.multiply(this.a, this.b));
+				break;
+			case 4:
+				System.out.println("Enter the two numbers to divide.");
+				getTwoNumbers();
+				System.out.println("The answer is: " + calculate.divide(this.a, this.b));
+				break;
+			case 5:
+				System.out.println("Enter variables: a, b, and c.");
+				System.out.println("Enter variable a: ");
+				this.a = input.nextDouble();
+				System.out.println("Enter variabe b: ");
+				this.b = input.nextDouble();
+				System.out.println("Enter variable c: ");
+				this.c = input.nextDouble();
+				input.nextLine();
+				Quadratic quadratic = new Quadratic(this.a, this.b, this.c);
+				System.out.println("Solution 1: " + quadratic.getA1());
+				System.out.println("Solution 2: " + quadratic.getA2());
+				break;
+			case 6:
+				instructions.getInstructions();
+		}
+	}
+	public void getTwoNumbers() {
+		System.out.println("Enter number 1: ");
+		this.a = input.nextDouble();
+		System.out.println("Enter number 2:");
+		this.b = input.nextDouble();
+		input.nextLine();
+	}
+}
